d3875ba210e01157bfeeba232b493e69b26b8fc8
Angel <angeleduardo16688@gmail.com>
2023-10-13 18:55:06 -0700

    13/10/2023 Laravel (6-8)

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,18 @@
 <?php
 use Illuminate\Support\Facades\Route;
-
-
+use App\Http\Controllers\AlumnoController;
 use App\Http\Controllers\bienvenidoController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 route::get('/bienvenido', [bienvenidoController::class, 'bienvenido']);
+
+route::get('/alumnos', [AlumnoController::class, 'alumnos']);
+
+
+    
+
